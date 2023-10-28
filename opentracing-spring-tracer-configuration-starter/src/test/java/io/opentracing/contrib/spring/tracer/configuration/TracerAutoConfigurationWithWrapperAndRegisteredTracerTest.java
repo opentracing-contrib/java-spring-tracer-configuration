51d7e781372ba1077761a07ddc8bfa8ec06b56fe
Ahmed Hussein <ahmedhussein411@gmail.com>
2023-10-28 15:26:58 +0200

    bump spring and use AutoConfiguration.imports
    spring factories is deprecated by spring boot 2.7 and completely removed in 3.0+

--- opentracing-spring-tracer-configuration-starter/src/test/java/io/opentracing/contrib/spring/tracer/configuration/TracerAutoConfigurationWithWrapperAndRegisteredTracerTest.java
+++ opentracing-spring-tracer-configuration-starter/src/test/java/io/opentracing/contrib/spring/tracer/configuration/TracerAutoConfigurationWithWrapperAndRegisteredTracerTest.java
@@ -24,6 +24,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest(
@@ -32,6 +33,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
             TracerAutoConfigurationWithWrapperAndRegisteredTracerTest.SpringConfiguration.class,
             TestTracerBeanPostProcessor.class})
 @RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext
 public class TracerAutoConfigurationWithWrapperAndRegisteredTracerTest extends BaseTest {
 
   @BeforeClass
